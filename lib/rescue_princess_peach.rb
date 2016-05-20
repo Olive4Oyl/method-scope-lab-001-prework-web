a45c14b3d845508e430eac15094f010fab5d65dc
Olive4Oyl <simoy93@gmail.com>
2016-05-20 01:14:52 -0400

    Update rescue_princess_peach.rb

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,11 +1,12 @@
-status = "rescued"
-
-def rescue_princess_peach (status)
+def rescue_princess_peach
   status = "rescued"
-  puts "Hooray! Mario has #{status} Princess Peach."
+  puts "Hooray! Mario has rescued Princess Peach."
 end
 
-rescue_princess_peach(status)
-puts status
 
 
+
+## Comment the below back in to see method scope in action!
+
+#rescue_princess_peach
+#puts status
